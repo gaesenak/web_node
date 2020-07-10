09329740698e63ffa4296116f774e022d38e0f9c
yj-oh <holidayoh99@gmail.com>
2020-07-10 11:37:36 +0900

    [기타] server.js | 변수 선언 ES6 변경

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var router = require('./routes/main')(app);
+const router = require('./routes/main')(app);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 
-var server = app.listen(8888, function() {
+const server = app.listen(8888, function() {
         console.log("Express Server running at http://localhost:8888");
 });
 
